e24281b41855569543984cd13a137769e830d69b
alexrumrum <alexlee0603alex@gmail.com>
2020-12-04 11:11:35 +0800

    Update hw4.py

--- HW4/hw4.py
+++ HW4/hw4.py
@@ -4,84 +4,77 @@ import numpy as np
 train_file = './hw4_train.dat'
 test_file = './hw4_test.dat'
 prob_str = '-b 1'
+lambda_set = [-4, -2, 0, 2, 4]
 
 def get_str(s):
     return '-s 0 -c ' + str(1/(2*(10**s))) + ' -e 0.000001 -q'
 
-
 def main():
     train_data = np.loadtxt(train_file)
-    train_y, train_x = train_data[:,-1].tolist(), train_data[:,:-1]
-    for i in range(6):
-        train_x = np.append(train_x, train_x[:,i:6] * train_x[:,i:i+1], axis=1)
-    train_x = np.insert(train_x, 0, 1, axis=1).tolist()
-    prob = problem(train_y, train_x)
-    
     test_data = np.loadtxt(test_file)
-    test_y, test_x = test_data[:,-1].tolist(), test_data[:,:-1]
+    y_lst, x_lst = train_data[:,-1].tolist(), train_data[:,:-1]
     for i in range(6):
-        test_x = np.append(test_x, test_x[:,i:6] * test_x[:,i:i+1], axis=1)
-    test_x = np.insert(test_x, 0, 1, axis=1).tolist()
-    C = [-4, -2, 0, 2, 4]
+        x_lst = np.append(x_lst, x_lst[:,i:6] * x_lst[:,i:i+1], axis=1)
+    x_lst = np.insert(x_lst, 0, 1, axis=1).tolist()
+    test_y_lst, test_x_lst = test_data[:,-1].tolist(), test_data[:,:-1]
+    for i in range(6):
+        test_x_lst = np.append(test_x_lst, test_x_lst[:,i:6] * test_x_lst[:,i:i+1], axis=1)
+    test_x_lst = np.insert(test_x_lst, 0, 1, axis=1).tolist()
     
+    problem_16_17_19 = problem(y_lst, x_lst)
     print("Problem 16")
-    for c in C:
+    for c in lambda_set:
         print(f"Lambda : {c}")
-        param = parameter(get_str(c))
-        m = train(prob, param)
-        p_label, p_acc, p_val = predict(test_y, test_x, m, prob_str)
-        ACC, MSE, SCC = evaluations(test_y, p_label)
+        p_16 = parameter(get_str(c))
+        m_16 = train(problem_16_17_19, p_16)
+        predict(test_y_lst, test_x_lst, m_16, prob_str)
     
     print("Problem 17")
-    for c in C:
+    for c in lambda_set:
         print(f"Lambda : {c}")
-        param = parameter(get_str(c))
-        m = train(prob, param)
-        p_label, p_acc, p_val = predict(train_y, train_x, m, prob_str)
-        ACC, MSE, SCC = evaluations(train_y, p_label)
+        p_17 = parameter(get_str(c))
+        m_17 = train(problem_16_17_19, p_17)
+        predict(y_lst, x_lst, m_17, prob_str)
     
     print("Problem 18")
-    train120_y, train120_x = train_y[:120], train_x[:120]
-    prob = problem(train120_y, train120_x)
-    val_y, val_x = train_y[120:], train_x[120:]
-    l = 0
-    M = None
-    acc = 0
-    for c in C:
+    train_y_lst_120, train_x_lst_120 = y_lst[:120], x_lst[:120]
+    problem_18 = problem(train_y_lst_120, train_x_lst_120)
+    validation_y_lst, validation_x_lst = y_lst[120:], x_lst[120:]
+    c_min, acc_min, model_min = 0, 0, None
+    for c in lambda_set:
         print(f"Lambda : {c}")
         param = parameter(get_str(c))
-        m = train(prob, param)
-        p_label, p_acc, p_val = predict(val_y, val_x, m, prob_str)
-        ACC, MSE, SCC = evaluations(val_y, p_label)
-        if ACC >= acc:
-            l, M, acc = c, m, ACC
-    p_label, p_acc, p_val = predict(test_y, test_x, M, prob_str)
-    ACC, MSE, SCC = evaluations(test_y, p_label)
+        m = train(problem_18, param)
+        p_label, _, _ = predict(validation_y_lst, validation_x_lst, m, prob_str)
+        ACC, _, _ = evaluations(validation_y_lst, p_label)
+        if ACC >= acc_min:
+            c_min, model_min, acc_min = c, m, ACC
+    p_label, _, _ = predict(test_y_lst, test_x_lst, model_min, prob_str)
+    ACC, _, _ = evaluations(test_y_lst, p_label)
     
     print("Problem 19")
-    prob = problem(train_y, train_x)
-    print(f"Lambda : {l}")
-    param = parameter(get_str(l))
-    m = train(prob, param)
-    p_label, p_acc, p_val = predict(test_y, test_x, m, prob_str)
-    ACC, MSE, SCC = evaluations(test_y, p_label)
+    print(f"Lambda : {c_min}")
+    param = parameter(get_str(c_min))
+    #run with full training set
+    m = train(problem_16_17_19, param)
+    predict(test_y_lst, test_x_lst, m, prob_str)
 
     print("Problem 20")
-    acc = 0
-    for c in C:
-        aacc = 0
+    s = 0
+    for c in lambda_set:
+        count = 0
         for i in range(0, 200, 40):
-            print(f"Lambda : {c}")
-            fold_y, fold_x = train_y[i:i+40], train_x[i:i+40]
-            prob = problem(train_y[0:i] + train_y[i+40:], train_x[0:i] + train_x[i+40:])
+            print(f"Lambda : {c}, Fold : {int(i/40)+1}")
+            fold_y_lst, fold_x_lst = y_lst[i:i+40], x_lst[i:i+40]
+            prob = problem(y_lst[0:i] + y_lst[i+40:], x_lst[0:i] + x_lst[i+40:])
             param = parameter(get_str(c))
             m = train(prob, param)
-            p_label, p_acc, p_val = predict(fold_y, fold_x, m, prob_str)
-            ACC, MSE, SCC = evaluations(fold_y, p_label)
-            aacc += ACC
-        if aacc >= acc:
-            acc = aacc
-    print(acc/5)
+            p_label, _, _ = predict(fold_y_lst, fold_x_lst, m, prob_str)
+            ACC, _, _ = evaluations(fold_y_lst, p_label)
+            count += ACC
+        if count >= s:
+            s = count
+    print(f"Best accuracy : {s/5}")
 
 if __name__ == "__main__":
     main()
