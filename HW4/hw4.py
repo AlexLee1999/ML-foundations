3198b6f4cf66084d5ef3eac557c229966f2c0da6
alexrumrum <alexlee0603alex@gmail.com>
2020-12-16 11:00:53 +0800

    Update hw4.py

--- HW4/hw4.py
+++ HW4/hw4.py
@@ -9,32 +9,35 @@ lambda_set = [-4, -2, 0, 2, 4]
 def get_str(s):
     return '-s 0 -c ' + str(1/(2*(10**s))) + ' -e 0.000001 -q'
 
+
+def prepare_file(file):
+    data = np.loadtxt(file)
+    y, x = data[:,-1].tolist(), data[:,:-1]
+    for i in range(6):
+        x = np.append(x, x[:,i:6] * x[:,i:i+1], axis=1)
+    x = np.insert(x, 0, 1, axis=1).tolist()
+    return x, y
+
+def solve(l, problem, x, y):
+    p = parameter(get_str(l))
+    m = train(problem, p)
+    p_L, _, _ = predict(y, x, m, prob_str)
+    accuracy, _, _ = evaluations(y, p_L)
+    return accuracy, m
+
 def main():
-    train_data = np.loadtxt(train_file)
-    test_data = np.loadtxt(test_file)
-    y_lst, x_lst = train_data[:,-1].tolist(), train_data[:,:-1]
-    for i in range(6):
-        x_lst = np.append(x_lst, x_lst[:,i:6] * x_lst[:,i:i+1], axis=1)
-    x_lst = np.insert(x_lst, 0, 1, axis=1).tolist()
-    test_y_lst, test_x_lst = test_data[:,-1].tolist(), test_data[:,:-1]
-    for i in range(6):
-        test_x_lst = np.append(test_x_lst, test_x_lst[:,i:6] * test_x_lst[:,i:i+1], axis=1)
-    test_x_lst = np.insert(test_x_lst, 0, 1, axis=1).tolist()
-    
+    x_lst, y_lst = prepare_file(train_file)
+    test_x_lst, test_y_lst = prepare_file(test_file)
     problem_16_17_19 = problem(y_lst, x_lst)
     print("Problem 16")
     for c in lambda_set:
         print(f"Lambda : {c}")
-        p_16 = parameter(get_str(c))
-        m_16 = train(problem_16_17_19, p_16)
-        predict(test_y_lst, test_x_lst, m_16, prob_str)
+        solve(c, problem_16_17_19, test_x_lst, test_y_lst)
     
     print("Problem 17")
     for c in lambda_set:
         print(f"Lambda : {c}")
-        p_17 = parameter(get_str(c))
-        m_17 = train(problem_16_17_19, p_17)
-        predict(y_lst, x_lst, m_17, prob_str)
+        solve(c, problem_16_17_19, x_lst, y_lst)
     
     print("Problem 18")
     train_y_lst_120, train_x_lst_120 = y_lst[:120], x_lst[:120]
@@ -43,35 +46,25 @@ def main():
     c_min, acc_min, model_min = 0, 0, None
     for c in lambda_set:
         print(f"Lambda : {c}")
-        param = parameter(get_str(c))
-        m = train(problem_18, param)
-        p_label, _, _ = predict(validation_y_lst, validation_x_lst, m, prob_str)
-        ACC, _, _ = evaluations(validation_y_lst, p_label)
-        if ACC >= acc_min:
-            c_min, model_min, acc_min = c, m, ACC
+        acc, m_18 = solve(c, problem_18, validation_x_lst, validation_y_lst)
+        if acc >= acc_min:
+            c_min, model_min, acc_min = c, m_18, acc
     p_label, _, _ = predict(test_y_lst, test_x_lst, model_min, prob_str)
-    ACC, _, _ = evaluations(test_y_lst, p_label)
     
     print("Problem 19")
     print(f"Lambda : {c_min}")
-    param = parameter(get_str(c_min))
-    #run with full training set
-    m = train(problem_16_17_19, param)
-    predict(test_y_lst, test_x_lst, m, prob_str)
+    solve(c_min, problem_16_17_19, test_x_lst, test_y_lst)
 
     print("Problem 20")
     s = 0
     for c in lambda_set:
         count = 0
-        for i in range(0, 200, 40):
-            print(f"Lambda : {c}, Fold : {int(i/40)+1}")
-            fold_y_lst, fold_x_lst = y_lst[i:i+40], x_lst[i:i+40]
-            prob = problem(y_lst[0:i] + y_lst[i+40:], x_lst[0:i] + x_lst[i+40:])
-            param = parameter(get_str(c))
-            m = train(prob, param)
-            p_label, _, _ = predict(fold_y_lst, fold_x_lst, m, prob_str)
-            ACC, _, _ = evaluations(fold_y_lst, p_label)
-            count += ACC
+        for i in range(5):
+            print(f"Lambda : {c}, Fold : {i+1}")
+            fold_y_lst, fold_x_lst = y_lst[i*40:(i+1)*40], x_lst[i*40:(i+1)*40]
+            problem_20 = problem(y_lst[0:i*40] + y_lst[(i+1)*40:], x_lst[0:i*40] + x_lst[(i+1)*40:])
+            acc, _ = solve(c, problem_20, fold_x_lst, fold_y_lst)
+            count += acc
         if count >= s:
             s = count
     print(f"Best accuracy : {s/5}")
